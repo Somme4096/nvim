187eac66812c48a1b6c0617df78f6b94f51df848
Somme4096 <somme4096@gmail.com>
2024-09-10 20:53:46 +0800

    feat: added gruvbox-material colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -31,7 +31,16 @@ return {
       })
     end,
   },
-
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
   {
     "folke/tokyonight.nvim",
     lazy = false,
