39dbb24d6a3680bea1e9e180e8b4c59c94e9a3cd
Somme4096 <somme4096@gmail.com>
2025-01-09 01:26:20 +0800

    feat: disable edgy.nvim and readded telescope as default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- LazyVim Extra Settings
+--
+vim.g.lazyvim_picker = "telescope"
+
 -- Disable relative line number
 vim.opt.relativenumber = false
 -- italics
