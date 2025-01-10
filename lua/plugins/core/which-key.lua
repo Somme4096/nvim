80a59878c2775af72ce1999be4b86a59c148cfce
Somme4096 <somme4096@gmail.com>
2025-01-10 18:13:33 +0000

    feat: Bring back "Classic" which-key style

---
+++ lua/plugins/core/which-key.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = "classic",
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {},
+  },
+}
