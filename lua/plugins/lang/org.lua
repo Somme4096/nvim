f799826c0ebdc9982667bd0c3627df40ff460d34
Søm <somme4096@gmail.com>
2025-09-05 16:35:08 +0800

    feat: renewed org support

--- lua/plugins/lang/org.lua
+++ lua/plugins/lang/org.lua
@@ -2,12 +2,23 @@ return {
   {
     "nvim-orgmode/orgmode",
     event = "VeryLazy",
+    branch = "master",
     ft = { "org" },
     config = function()
       -- Setup orgmode
       require("orgmode").setup({
-        org_agenda_files = "~/orgfiles/**/*",
-        org_default_notes_file = "~/orgfiles/refile.org",
+        org_agenda_files = "~/Documents/**/*",
+        org_default_notes_file = "~/Documents/org/Inbox.org",
+        org_startup_folded = "showeverything",
+        org_startup_indented = false,
+        org_todo_keywords = { "TODO", "DOING", "NEXT", "WAIT", "|", "DROP", "DONE" },
+        org_capture_templates = {
+          j = {
+            description = "daily",
+            template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+            target = "~/Documents/org/Inbox.org",
+          },
+        },
       })
 
       -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
@@ -16,6 +27,13 @@ return {
       --   ensure_installed = 'all',
       --   ignore_install = { 'org' },
       -- })
+    end,
+  },
+  {
+    "akinsho/org-bullets.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("org-bullets").setup()
     end,
   },
 }
